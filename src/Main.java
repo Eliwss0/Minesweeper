74851a90e646dcc556936c84f9962fb255b7a00b
Alexander <me@alexanderrohrig.com>
2018-11-14 17:03:20 -0600

    Added main window & win/lose dialog box

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Main {
+
     public static void main(String[] args){
-        System.out.println("Hello World");
+
+        JFrame appFrame = new JFrame();
+        appFrame.setSize(500, 500);
+        appFrame.setTitle("Minesweeper");
+        appFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        appFrame.setVisible(true);
+
     }
+
+    public static void showWinLoseAlert(String message, String title) {
+        JOptionPane.showMessageDialog(null, message, title, JOptionPane.INFORMATION_MESSAGE);
+    }
+
 }
