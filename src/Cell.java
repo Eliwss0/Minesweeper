5ad004f39303e0749bce7d4c4bf1c6ff8291767b
Eli Weiss <eliwss0@gmail.com>
2018-11-28 17:05:38 -0600

    Moved cell checks to Grid.java, implemented adjacent reveal recursion

--- src/Cell.java
+++ src/Cell.java
@@ -6,21 +6,22 @@ class Cell extends JPanel {
     private boolean hasMine;
     private boolean isFlagged;
     private boolean isChecked;
-    private int numOfAdjMines, x, y;
+    private int numOfAdjMines,x,y;
     Cell(){
         hasMine=false;
         isFlagged=false;
         numOfAdjMines=0;
-
         addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
                 if (SwingUtilities.isLeftMouseButton(e)) {
                     // left click
+                    //check
                 }
                 else if (SwingUtilities.isRightMouseButton(e)) {
                     // right click
+                    //set flag
                 }
             }
         }
@@ -38,27 +39,13 @@ class Cell extends JPanel {
     void setChecked(){
         isChecked=true;
         if(hasMine) {
-            //TODO: Game over
             Main.showWinLoseAlert("You have lost.", "Game Over");
         }
     }
-    int checkForAdjMines(Cell[][] arr,int x,int y){
-        if(arr[y][x+1].hasMine) //check right
-            numOfAdjMines++;
-        if(arr[y+1][x].hasMine) //check up
-            numOfAdjMines++;
-        if(arr[y+1][x+1].hasMine) //check up-right
-            numOfAdjMines++;
-        if(arr[y-1][x].hasMine) //check down
-            numOfAdjMines++;
-        if(arr[y][x-1].hasMine) //check left
-            numOfAdjMines++;
-        if(arr[y-1][x-1].hasMine) //check down-left
-            numOfAdjMines++;
-        if(arr[y+1][x-1].hasMine) //check up-left
-            numOfAdjMines++;
-        if(arr[y-1][x+1].hasMine) //check down-right
-            numOfAdjMines++;
+    void setNumOfAdjMines(int numMines){
+        numOfAdjMines=numMines;
+    }
+    int getNumOfAdjMines(){
         return numOfAdjMines;
     }
 }
--- src/Grid.java
+++ src/Grid.java
@@ -8,11 +8,46 @@ public class Grid {
         while(i<15){
             randX=(int)(Math.random()*10);
             randY=(int)(Math.random()*10);
-            //not working check to see if mine is already there
-//            if(!cellsList[randY][randX].getHasMine()){
-//                cellsList[randY][randX].setMine();
-//                i++;
-//            }
+            i++;
         }
+    }
+    int checkForAdjMines(Cell[][] arr,int x,int y){
+        int numOfAdjMines=0;
+        if(arr[y][x+1].getHasMine()) //check right
+            numOfAdjMines++;
+        if(arr[y+1][x].getHasMine()) //check up
+            numOfAdjMines++;
+        if(arr[y+1][x+1].getHasMine()) //check up-right
+            numOfAdjMines++;
+        if(arr[y-1][x].getHasMine()) //check down
+            numOfAdjMines++;
+        if(arr[y][x-1].getHasMine()) //check left
+            numOfAdjMines++;
+        if(arr[y-1][x-1].getHasMine()) //check down-left
+            numOfAdjMines++;
+        if(arr[y+1][x-1].getHasMine()) //check up-left
+            numOfAdjMines++;
+        if(arr[y-1][x+1].getHasMine()) //check down-right
+            numOfAdjMines++;
+        return numOfAdjMines;
+    }
+    void revealAdj(Cell[][] arr,int y,int x){
+        //woo recursion
+        if (arr[y][x + 1].getNumOfAdjMines() == 0) //check right
+            revealAdj(cellsList,y,x+1);
+        if (arr[y + 1][x].getNumOfAdjMines() == 0) //check up
+            revealAdj(cellsList,y+1,x);
+        if (arr[y + 1][x + 1].getNumOfAdjMines() == 0) //check up-right
+            revealAdj(cellsList,y+1,x+1);
+        if (arr[y - 1][x].getNumOfAdjMines() == 0) //check down
+            revealAdj(cellsList,y-1,x);
+        if (arr[y][x - 1].getNumOfAdjMines() == 0) //check left
+            revealAdj(cellsList,y,x-1);
+        if (arr[y - 1][x - 1].getNumOfAdjMines() == 0) //check down-left
+            revealAdj(cellsList,y-1,x-1);
+        if (arr[y + 1][x - 1].getNumOfAdjMines() == 0) //check up-left
+            revealAdj(cellsList,y+1,x-1);
+        if (arr[y - 1][x + 1].getNumOfAdjMines() == 0) //check down-right
+            revealAdj(cellsList,y-1,x+1);
     }
 }
